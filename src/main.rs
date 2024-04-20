408c0747eff9bae760876496fa91b2ae0e030888
lylephone <lylephone@users.noreply.github.com>
2024-04-20 14:43:01 +0900

    get oidc metadata

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+use std::error::Error;
 use std::{env, process};
 
+use openidconnect::reqwest::http_client;
+use openidconnect::{core::CoreProviderMetadata, IssuerUrl};
 fn main() {
-
     let issuer_key = "OIDC_ISSUER";
 
     let issuer = match env::var(issuer_key) {
-        Ok(val) =>val,
+        Ok(val) => val,
         Err(err) => {
-            println!("Error: {}:{}",err,issuer_key);
+            println!("Error: {}:{}", err, issuer_key);
             process::exit(1);
-        },
-
+        }
     };
-    println!("Issuer is {}",issuer);
-    println!("Hello, world!");
+    println!("Issuer is {}", issuer);
+    let s = oidc(issuer);
+
+    println!("{:?}", s)
+}
+
+fn oidc(issuer: String) -> Result<(), Box<dyn Error>> {
+    let provider_metadata = CoreProviderMetadata::discover(&IssuerUrl::new(issuer)?, http_client)?;
+    print!("{:?}", provider_metadata);
+
+    Ok(())
 }
