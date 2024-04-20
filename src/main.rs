74f90da772cfca38a00da3e366bf1092445b54f2
lylephone <lylephone@users.noreply.github.com>
2024-04-20 13:52:39 +0900

    add env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, process};
+
 fn main() {
+
+    let issuer_key = "OIDC_ISSUER";
+
+    let issuer = match env::var(issuer_key) {
+        Ok(val) =>val,
+        Err(err) => {
+            println!("Error: {}:{}",err,issuer_key);
+            process::exit(1);
+        },
+
+    };
+    println!("Issuer is {}",issuer);
     println!("Hello, world!");
 }
